360273b5d3e7352bdbc5b48ab5e51b3bc0449b41
Dougy Fresh <dougrouseltw@gmail.com>
2023-11-21 21:22:43 -0700

    added js code

---
+++ script.js
@@ -0,0 +1,70 @@
+// Function to handle form submission
+function search() {
+    // Get user input values
+    const distance = document.getElementById('distance').value;
+    const price = document.getElementById('price').value;
+    const foodType = document.getElementById('foodType').value;
+    const rating = document.getElementById('rating').value;
+    const radius = document.getElementById('radius').value;
+  
+    // You can use these values to make API calls or perform other actions
+    // For simplicity, let's just log the values for now
+    console.log('Search Criteria:');
+    console.log('Distance:', distance);
+    console.log('Price:', price);
+    console.log('Food Type:', foodType);
+    console.log('Rating:', rating);
+    console.log('Radius:', radius);
+  
+    // Call a function to select a random restaurant based on the criteria
+    selectRandomRestaurant();
+  }
+  
+  // Function to select a random restaurant (placeholder)
+  function selectRandomRestaurant() {
+    // In a real application, you would make API calls here to fetch restaurant data
+    // For now, let's just create a sample array of restaurants
+    const restaurants = [
+      { name: 'Restaurant A', rating: 4.5, distance: 3 },
+      { name: 'Restaurant B', rating: 3.8, distance: 5 },
+      { name: 'Restaurant C', rating: 4.2, distance: 2 },
+      // Add more restaurants as needed
+    ];
+  
+    // Filter restaurants based on user criteria (distance, rating, etc.)
+    const filteredRestaurants = restaurants.filter(restaurant => {
+      return restaurant.distance <= parseInt(document.getElementById('distance').value) &&
+             restaurant.rating >= parseInt(document.getElementById('rating').value);
+      // Add more criteria as needed
+    });
+  
+    // Select a random restaurant from the filtered list
+    const randomRestaurant = filteredRestaurants[Math.floor(Math.random() * filteredRestaurants.length)];
+  
+    // Display the selected restaurant's details (you can update your UI accordingly)
+    console.log('Randomly Selected Restaurant:');
+    console.log('Name:', randomRestaurant.name);
+    console.log('Rating:', randomRestaurant.rating);
+    console.log('Distance:', randomRestaurant.distance);
+  
+    // Save the result to local storage
+    saveResultToLocalStorage(randomRestaurant);
+  }
+  
+  // Function to save the result to local storage
+  function saveResultToLocalStorage(restaurant) {
+    // Retrieve existing results from local storage or initialize an empty array
+    const pastResults = JSON.parse(localStorage.getItem('pastResults')) || [];
+  
+    // Add the new result to the array
+    pastResults.push(restaurant);
+  
+    // Save the updated array back to local storage
+    localStorage.setItem('pastResults', JSON.stringify(pastResults));
+  
+    // Display the past results on the page (you can update your UI accordingly)
+    console.log('Past Results:', pastResults);
+  }
+  
+  // You can add more functions as needed and integrate with mapping APIs for a real application
+  
