b2b135a65f73312e45fea4c55d5d74306ad87288
Jesse Nay <jessenay26@gmail.com>
2023-12-04 18:57:25 -0700

    fixed a word in the autocomplete function

--- assets/script.js
+++ assets/script.js
@@ -177,7 +177,7 @@ const apiKey = 'AIzaSyBHeBzhIMst_moJaXl-g23xT55gjJ3_LiY';
 function initAutocomplete() {
   console.log(initAutocomplete)
   const input = document.getElementById('autocomplete');
-  const autocomplete = new google.maps.places.Autocomplete(input, { types: ['geocode'] });
+  const autocomplete = new google.maps.places.Autocomplete(input, { types: ['address'] });
   console.log(initAutocomplete)
 
   autocomplete.addListener('place_changed', function() {
